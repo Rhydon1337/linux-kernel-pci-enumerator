e99dccd6342850358dfab393bc295a220f994fb6
Rhydon <adirmak1@gmail.com>
2020-10-23 23:25:58 +0300

    fix indentations

--- main.c
+++ main.c
@@ -24,24 +24,24 @@ static const struct file_operations fops = {
 };
 
 static int pci_enumerator_init(void)
-{	
-	int err;
+{   
+    int err;
     dev_t dev;
-	printk(KERN_INFO "hello...\n");
-	err = alloc_chrdev_region(&dev, 0, MINOR_VERSION, DEVICE_NAME);
+    printk(KERN_INFO "hello...\n");
+    err = alloc_chrdev_region(&dev, 0, MINOR_VERSION, DEVICE_NAME);
     dev_major = MAJOR(dev);
-	
-	pci_enumerator_class = class_create(THIS_MODULE, DEVICE_NAME);
+    
+    pci_enumerator_class = class_create(THIS_MODULE, DEVICE_NAME);
 
-	cdev_init(&cdev, &fops);
-	cdev_add(&cdev, MKDEV(dev_major, MINOR_VERSION), 1);
-	device_create(pci_enumerator_class, NULL, MKDEV(dev_major, MINOR_VERSION), NULL, DEVICE_NAME);
-	return 0;
+    cdev_init(&cdev, &fops);
+    cdev_add(&cdev, MKDEV(dev_major, MINOR_VERSION), 1);
+    device_create(pci_enumerator_class, NULL, MKDEV(dev_major, MINOR_VERSION), NULL, DEVICE_NAME);
+    return 0;
 }
 
 static void pci_enumerator_exit(void)
 {
-	printk(KERN_INFO "bye...\n");
+    printk(KERN_INFO "bye...\n");
 }
 
 module_init(pci_enumerator_init);
--- pci_enumeration.c
+++ pci_enumeration.c
@@ -11,7 +11,7 @@ struct list_head* pci_enumrator(void) {
     size_t i, j;
     INIT_LIST_HEAD(pci_ids);
     j = 0;
-	list_for_each_entry(bus, &pci_root_buses, node) {
+    list_for_each_entry(bus, &pci_root_buses, node) {
         for (i = 0; i < MAX_NUMBER_OF_PCI_DEV_PER_BUS; i++) {
             u16 device_id = 0;
             u16 vendor_id = 0;
@@ -42,10 +42,10 @@ struct list_head* pci_enumrator(void) {
 void clean_up_list(struct list_head* pci_devices) {
     struct PciID* pci_id;
     struct list_head *pos, *q;
-    list_for_each_safe(pos, q, pci_devices){
-		pci_id = list_entry(pos, struct PciID, pci_ids_list);
-		list_del(pos);
+    list_for_each_safe(pos, q, pci_devices) {
+        pci_id = list_entry(pos, struct PciID, pci_ids_list);
+        list_del(pos);
         kfree(pci_id);
-	}
+    }
     kfree(pci_devices);
 }
